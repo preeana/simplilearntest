d342db2016a00d1c677e192ab3e4733091c9c30a
preeana <vpreethy@gmail.com>
2024-05-21 23:55:35 +0530

    made changes

--- src/test/java/simplilearnjenkin/LoginPg.java
+++ src/test/java/simplilearnjenkin/LoginPg.java
@@ -15,9 +15,10 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import io.github.bonigarcia.wdm.webdriver.*;
 
-@Test(priority=1)
+@Test
 public class LoginPg {
 
+	@Test(priority=1)
 	public  void test1() {
 		// TODO Auto-generated method stub
 		WebDriverManager.firefoxdriver().setup();
@@ -39,5 +40,26 @@ public class LoginPg {
 	    System.out.println(error_msg);
 	    }
 	}
+	@Test(priority=2)
+	public  void test2() {
+		// TODO Auto-generated method stub
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver=new FirefoxDriver();
+		
+		   	driver.get("https://accounts.simplilearn.com/user/login?redirect_url=https%3A%2F%2Flms.simplilearn.com%2F");
 
+	   WebElement UserName= driver.findElement(By.name("user_login"));
+	   UserName.sendKeys("vpreethy@gmail.com");
+	    WebElement Password= driver.findElement(By.id("password"));
+	    Password.sendKeys("Selinium@1236");
+	    //WebElement RememberMe = driver.findElement(By.className("rememberMe"));
+	    WebElement LoginBtn = driver.findElement(By.name("btn_login"));
+	    LoginBtn.click();
+	    WebElement error_msg= driver.findElement(By.id("msg_box"));
+	    String msg ="The email or password you have entered is invalid.";
+	    if(error_msg.equals(msg))
+	    {
+	    System.out.println(error_msg);
+	    }
+	}
 }
